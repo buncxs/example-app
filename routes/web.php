af9caf16eaac2c5c21153356865a1325dbf1c07c
Ivan miniPc <buncxs_2@hotmail.com>
2026-02-03 11:21:52 -0600

    template DONE

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,15 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
+/*
 Route::get('/', function () {
     return Inertia::render('Welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home'); */
+
+Route::get('/', function () {
+    return Inertia::render('auth/Login', [
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('home');
